44d4f916cb653574d69e3cf9bb63bdc591a11bb4
Dimitri Bouniol <dimitribouniol@mochidev.com>
2012-05-04 13:32:07 -0700

    first commit

---
+++ MDAccordianView.h
@@ -0,0 +1,13 @@
+//
+//  MDAccordianView.h
+//  MDAccordianViewDemo
+//
+//  Created by Dimitri Bouniol on 5/4/12.
+//  Copyright (c) 2012 Mochi Development, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDAccordianView : UIView
+
+@end
---
+++ MDAccordianView.m
@@ -0,0 +1,31 @@
+//
+//  MDAccordianView.m
+//  MDAccordianViewDemo
+//
+//  Created by Dimitri Bouniol on 5/4/12.
+//  Copyright (c) 2012 Mochi Development, Inc. All rights reserved.
+//
+
+#import "MDAccordianView.h"
+
+@implementation MDAccordianView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
